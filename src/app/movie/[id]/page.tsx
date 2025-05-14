2d86a8f6cb777407c74c29ebe860fe5c618a466f
alinikfarjam79 <alinikitest@gmail.com>
2025-05-14 09:55:54 +0330

    fixed

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,14 +1,17 @@
-import { Movie } from "@/types";
 import { fetchMovieDetails } from "@/utils/fetcher";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const movie: Movie = await fetchMovieDetails(params.id);
+export default async function MovieDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
 
+  const movie = await fetchMovieDetails(id);
   return (
     <div className="p-4">
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
         className="rounded w-full max-w-md"
       />
       <h1 className="text-3xl font-bold mt-4">{movie.title}</h1>
